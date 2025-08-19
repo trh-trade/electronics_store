760d3de1ddbd065654e27a951ca0cc43776b279c
Trh-trade <trhtrade18@gmail.com>
2025-08-19 12:49:36 +0200

    Add WWW redirect middleware and fix urls.py

--- electronics_store/settings.py
+++ electronics_store/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'electronics_store.middleware.RedirectToWWW',  # <- замените your_app на имя вашего приложения
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',  
     'django.contrib.sessions.middleware.SessionMiddleware',
--- electronics_store/urls.py
+++ electronics_store/urls.py
@@ -3,26 +3,9 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.shortcuts import redirect
-from django.http import HttpResponseNotFound
-
-def redirect_to_www(request):
-    host = request.get_host()
-    if host == 'trh-trade.sk':
-        return redirect(f"https://www.trh-trade.sk{request.get_full_path()}", permanent=True)
-    # Если хост не trh-trade.sk, возвращаем 404 (или можно пропускать дальше)
-    return HttpResponseNotFound("Not Found")
 
 urlpatterns = [
-    # Редирект всех запросов с корневого домена
-    re_path(r'^.*$', redirect_to_www),
-
-    # Админка
     path('admin/', admin.site.urls),
-
-    # Основное приложение
     path('', include('main.urls')),
-
-    # Медиа
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
