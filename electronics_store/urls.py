054e02bb9f35d262356e93a11864d29c7f636582
Trh-trade <trhtrade18@gmail.com>
2025-08-19 12:33:29 +0200

    Fix missing imports in urls.py

--- electronics_store/urls.py
+++ electronics_store/urls.py
@@ -3,9 +3,25 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.shortcuts import redirect
+
+# Функция редиректа с корня на www
+def redirect_to_www(request):
+    host = request.get_host()
+    if host == 'trh-trade.sk':
+        return redirect(f"https://www.trh-trade.sk{request.get_full_path()}", permanent=True)
+    return None
 
 urlpatterns = [
+    # Редирект всех запросов с корневого домена (срабатывает первым)
+    re_path(r'^.*$', redirect_to_www),
+
+    # Админка Django
     path('admin/', admin.site.urls),
+
+    # Подключение основного приложения
     path('', include('main.urls')),
+
+    # Раздача медиа-файлов
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
